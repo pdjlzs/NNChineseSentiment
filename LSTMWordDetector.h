c77c46d4f22561067f1f9800e0b202bc0ae21cd7
Da Pan <pdjzs@live.com>
2016-03-09 21:16:20 +0800

    feat: add nbest of single segStyle file

---
+++ LSTMWordDetector.h
@@ -0,0 +1,82 @@
+/*
+ * Labeler.h
+ *
+ *  Created on: Mar 16, 2015
+ *      Author: mszhang
+ */
+
+#ifndef SRC_Detector_H_
+#define SRC_Detector_H_
+
+#include <set>
+
+#include "Options.h"
+#include "Instance.h"
+#include "ExampleMultiSeg.h"
+#include "Pipe.h"
+#include "Utf.h"
+#include "N3L.h"
+
+#include "basic/LSTMCNNWordClassifier.h"
+
+using namespace nr;
+using namespace std;
+
+class Labeler {
+
+public:
+  std::string nullkey;
+  std::string unknownkey;
+  std::string seperateKey;
+
+public:
+  Alphabet m_labelAlphabet;
+  Alphabet m_featAlphabet;
+  Alphabet m_wordAlphabet;
+  Alphabet m_charAlphabet;
+  Alphabet m_segStylelabelAlphabet;
+
+public:
+  Options m_options;
+
+  Pipe m_pipe;
+
+  int m_linearfeat;
+
+#if USE_CUDA==1
+  LSTMCNNWordClassifier<gpu> m_classifier;
+#else
+  LSTMCNNWordClassifier<cpu> m_classifier;
+#endif
+
+public:
+  void readWordEmbeddings(const string& inFile, NRMat<dtype>& wordEmb);
+
+public:
+  Labeler();
+  virtual ~Labeler();
+
+public:
+
+  int createAlphabet(const vector<Instance>& vecInsts);
+
+  int addTestWordAlpha(const vector<Instance>& vecInsts);
+  int addTestCharAlpha(const vector<Instance>& vecInsts);
+
+  void extractFeature(vector<Feature> & feat, const Instance* pInstance, int idx);
+  void extractLinearFeatures(vector<string>& features, const Instance* pInstance);
+  void convert2Example(const Instance* pInstance, Example& exam);
+  void initialExamples(const vector<Instance>& vecInsts, vector<Example> & vecExams);
+
+public:
+  void train(const string& trainFile, const string& devFile, const string& testFile, const string& modelFile, const string& optionFile,
+      const string& wordEmbFile);
+  dtype predict(const vector<int>& linears, const vector<vector<Feature> >& features, string& output);
+  void test(const string& testFile, const string& outputFile, const string& modelFile);
+
+  void writeModelFile(const string& outputModelFile);
+  void loadModelFile(const string& inputModelFile);
+
+};
+
+#endif /* SRC_Detector_H_ */
